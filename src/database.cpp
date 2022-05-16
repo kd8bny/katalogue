ba6dab573a656643b52cca68bbf6b0720612d4d4
Daryl Bennett <kd8bny@gmail.com>
2022-05-16 19:18:15 -0400

    Fix FKEY

--- src/database.cpp
+++ src/database.cpp
@@ -37,12 +37,11 @@ QString year, QString category, QString group)
     QString uuid = QUuid().createUuid().toString();
 
     query.prepare("INSERT INTO " TABLE_ITEMS " ("
-        TABLE_UUID ", " TABLE_NAME ", " TABLE_MAKE ", " TABLE_MODEL ", "
-        TABLE_YEAR ", " TABLE_CATEGORY ", " TABLE_GROUP ", " TABLE_ARCHIVED")"
-        " VALUES (:uuid, :name, :make, :model, :year, :category, "
+        TABLE_NAME ", " TABLE_MAKE ", " TABLE_MODEL ", " TABLE_YEAR ", "
+        TABLE_CATEGORY ", " TABLE_GROUP ", " TABLE_ARCHIVED")"
+        " VALUES (:name, :make, :model, :year, :category, "
         ":view, :archived)");
 
-    query.bindValue(":uuid", uuid);
     query.bindValue(":name", name);
     query.bindValue(":make", make);
     query.bindValue(":model", model);
@@ -54,26 +53,25 @@ QString year, QString category, QString group)
     if(query.exec()){
         isInsert = true;
     } else {
-        qDebug() << "Error inserting record " << TABLE_ITEMS;
-        qDebug() << query.lastError().text();
+        qDebug() << "Error inserting record " << query.lastError().text();
     }
 
     return isInsert;
 }
 
-bool Database::insertAttributeEntry(QString uuid, QString label, QString key, QString value)
+bool Database::insertAttributeEntry(QString label, QString key, QString value, QString item_id)
 {
     bool isInsert = false;
     QSqlQuery query;
 
     query.prepare("INSERT INTO " TABLE_ATTRIBUTES " ("
-        TABLE_UUID ", " TABLE_LABEL ", " TABLE_KEY ", " TABLE_VALUE ")"
-        " VALUES (:uuid, :label, :key, :value)");
+        TABLE_LABEL ", " TABLE_KEY ", " TABLE_VALUE  ", " TABLE_ITEM_ID")"
+        " VALUES (:label, :key, :value, :item_id)");
 
-    query.bindValue(":uuid", uuid);
     query.bindValue(":label", label);
     query.bindValue(":key", key); 
     query.bindValue(":value", value);
+    query.bindValue(":item_id", item_id);
 
     if(query.exec()){
         isInsert = true;
@@ -85,24 +83,24 @@ bool Database::insertAttributeEntry(QString uuid, QString label, QString key, QS
     return isInsert;
 }
 
-bool Database::insertEventEntry(QString uuid, QString date, QString task,
-                                QString cost, QString type, QString category, QString comment)
+bool Database::insertEventEntry(QString date, QString task, QString cost, 
+    QString type, QString category, QString comment, QString item_id)
 {
     bool isInsert = false;
     QSqlQuery query;
 
     query.prepare("INSERT INTO " TABLE_EVENTS " ("
-        TABLE_UUID ", " TABLE_DATE ", " TABLE_TASK ", " TABLE_COST ", "
-        TABLE_TYPE ", " TABLE_CATEGORY  ", " TABLE_COMMENT")"
-        " VALUES (:uuid, :date, :task, :cost, :type, :category, :comment)");
+        TABLE_DATE ", " TABLE_TASK ", " TABLE_COST ", "
+        TABLE_TYPE ", " TABLE_CATEGORY  ", " TABLE_COMMENT ", " TABLE_ITEM_ID")"
+        " VALUES (:date, :task, :cost, :type, :category, :comment, :item_id)");
 
-    query.bindValue(":uuid", uuid);
     query.bindValue(":date", date);
     query.bindValue(":task", task);
     query.bindValue(":cost", cost);
     query.bindValue(":type", type);
     query.bindValue(":category", category);
-    query.bindValue(":comment", comment); 
+    query.bindValue(":comment", comment);
+    query.bindValue(":item_id", item_id);
 
     if(query.exec()){
         isInsert = true;
@@ -122,7 +120,6 @@ bool Database::initializeSchema()
 
     const QString queryItems = "CREATE TABLE " TABLE_ITEMS
         " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        TABLE_UUID      " TEXT NOT NULL, "
         TABLE_NAME      " TEXT NOT NULL, "
         TABLE_MAKE      " TEXT, "
         TABLE_MODEL     " TEXT, "
@@ -133,20 +130,22 @@ bool Database::initializeSchema()
 
     const QString queryAttributes = "CREATE TABLE " TABLE_ATTRIBUTES
         " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        TABLE_UUID      " TEXT NOT NULL, "
         TABLE_LABEL     " TEXT NOT NULL, "
         TABLE_KEY       " TEXT NOT NULL, "
-        TABLE_VALUE     " TEXT NOT NULL)";
+        TABLE_VALUE     " TEXT NOT NULL, "
+        TABLE_ITEM_ID   " INT NOT NULL, "
+        "FOREIGN KEY (" TABLE_ITEM_ID ") REFERENCES " TABLE_ITEMS "(id) )";
 
     const QString queryEvent = "CREATE TABLE " TABLE_EVENTS
         " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        TABLE_UUID      " TEXT NOT NULL, "
         TABLE_DATE      " DATE NOT NULL, "
         TABLE_TASK      " TEXT NOT NULL, "
         TABLE_COST      " REAL, "
         TABLE_TYPE      " TEXT, "
         TABLE_CATEGORY  " TEXT, "
-        TABLE_COMMENT   " VARCHAR(255))";
+        TABLE_COMMENT   " VARCHAR(255), "
+        TABLE_ITEM_ID   " INT NOT NULL, "
+        "FOREIGN KEY (" TABLE_ITEM_ID ") REFERENCES " TABLE_ITEMS "(id) )";
 
     bool isItems = false;
     bool isAttributes = false;
@@ -161,6 +160,7 @@ bool Database::initializeSchema()
     if(query.exec(queryAttributes)){
         isAttributes = true;
     }else{
+        qDebug() << queryAttributes;
         qDebug() << query.lastError();
     }
 
@@ -183,15 +183,7 @@ void Database::initializeDemoEntry()
     this->insertItemEntry(
         "My Vehicle", "Ford", "Mustang", "2000", "auto", "My Group");
 
-    QSqlQuery query;
-    query.prepare("SELECT " TABLE_UUID " FROM " TABLE_ITEMS " WHERE id=1");
-    if(!query.exec()){
-        qDebug() << query.lastError();
-    }
-    query.next();
-    QString uuid = query.value(0).toString();
-
-    this->insertAttributeEntry(uuid, "Default", "Engine", "4.6L V8");
-    this->insertEventEntry(uuid, "2022-05-22", "Check Oil", "0.00", "checkup",
-        "auto", "Comment about event");
+    this->insertAttributeEntry("Default", "Engine", "4.6L V8", "1");
+    this->insertEventEntry("2022-05-22", "Check Oil", "0.00", "checkup",
+        "auto", "Comment about event", "1");
 }
