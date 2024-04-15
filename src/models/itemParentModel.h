3e2e4d264161b43c410b7323dccea9f3fc6b2a54
Daryl Bennett <kd8bny@gmail.com>
2024-04-14 19:25:47 -0500

    Ignore archived items in parentmodel

--- src/models/itemParentModel.h
+++ src/models/itemParentModel.h
@@ -17,9 +17,10 @@ public:
 
 private:
     // First select is the combox values, second is the Id of the record
+    // Ignore components and archived Items
     const QString modelQuery = QStringLiteral(
-        "SELECT DISTINCT %1, id FROM %2 WHERE %3 IS NULL").arg(
-            Database::NAME, Database::TABLE_ITEMS, Database::KEY_ITEM_ID);
+        "SELECT DISTINCT %1, id FROM %2 WHERE %3 IS NULL AND %4 IS 0").arg(
+            Database::NAME, Database::TABLE_ITEMS, Database::KEY_ITEM_ID, Database::ARCHIVED);
 
 Q_SIGNALS:
     void dataChanged();
