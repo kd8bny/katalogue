5b577095266805bed659ccfe51a13df442a41f04
Daryl Bennett <kd8bny@gmail.com>
2024-07-29 23:01:55 -0500

    update tests

--- tests/databaseEventTest.cpp
+++ tests/databaseEventTest.cpp
@@ -10,7 +10,7 @@ class DatabaseEventTest : public QObject
 
 private Q_SLOTS:
     void insertEventEntry() const;
-    // void updateEventEntry() const;
+    void updateEventEntry() const;
     // void deleteEventEntry() const;
 };
 
@@ -83,70 +83,97 @@ void DatabaseEventTest::insertEventEntry() const
     }
 }
 
-// void DatabaseEventTest::updateEventEntry() const
-// {
-//     /*Test Event Insert*/
-//     Database katalogue_db;
-//     bool DB_OPEN = katalogue_db.connectKatalogueDb(this->testDBPath);
-//     qDebug() << "db open" << DB_OPEN;
-//     QVERIFY(DB_OPEN == true);
+void DatabaseEventTest::updateEventEntry() const
+{
+    /*Test Event Insert*/
+    Database katalogue_db;
+    bool DB_OPEN = katalogue_db.connectKatalogueDb(this->testDBPath);
+    qDebug() << "db open" << DB_OPEN;
+    QVERIFY(DB_OPEN == true);
 
-//     Event refEvent(-1);
-//     refEvent.setKey(QStringLiteral("refKey"));
-//     refEvent.setValue(QStringLiteral("refValue"));
-//     refEvent.setCategory(QStringLiteral("refCat"));
-//     refEvent.setItemId(1);
+    Event refEvent(-1);
+    refEvent.setDate(QStringLiteral("2015-09-19T00:00:00.000-05:00"));
+    refEvent.setEvent(QStringLiteral("refEvent"));
+    refEvent.setCost(123.45);
+    refEvent.setIncrement(123456.7);
+    refEvent.setCategory(QStringLiteral("category"));
+    refEvent.setComment(QStringLiteral("comment"));
+    refEvent.setItemId(1);
 
-//     QVERIFY2(katalogue_db.insertEventEntry(refEvent) == true, "Reference Event added");
+    QVERIFY2(katalogue_db.insertEventEntry(refEvent) == true, "Reference Event added");
 
-//     QVariantList eventFields = {
-//         QStringLiteral("refKeyUpdated"),
-//         QStringLiteral("refValueUpdated"),
-//         QStringLiteral("refCatUpdated"),
-//         4,
-//     };
+    QVariantList eventFields = {QStringLiteral("2222-09-19T00:00:00.000-05:00"), QStringLiteral("eventUpdated"),
+                                QStringLiteral("567.89"), QStringLiteral("765432.1"), QStringLiteral("categoryUpdated"),
+                                QStringLiteral("commentUpdated"), 1};
 
-//     // Query for reference event id 3
-//     const QString record3Query = QStringLiteral("SELECT %1, %2, %3, %4 FROM %5 WHERE id=3")
-//                                      .arg(Database::KEY, Database::VALUE, Database::CATEGORY,
-//                                           Database::KEY_ITEM_ID, Database::TABLE_ATTRIBUTES);
-//     QSqlQuery query;
-//     query.exec(record3Query);
-//     query.next();
-//     // qDebug() << query.record();
+    // Query for reference event id 3
+    const QString record3Query = QStringLiteral("SELECT %1, %2, %3, %4, %5, %6 FROM %7 WHERE id=3")
+                                     .arg(Database::DATE, Database::EVENT, Database::COST, Database::INCREMENT,
+                                          Database::CATEGORY, Database::COMMENT, Database::TABLE_EVENTS);
+    QSqlQuery query;
+    query.exec(record3Query);
+    query.next();
+    qDebug() << query.record();
 
-//     // Build Event 3
-//     Event event3(3);
-//     event3.setKey(query.value(0).toString());
-//     event3.setValue(query.value(1).toString());
-//     event3.setCategory(query.value(2).toString());
-//     event3.setItemId(query.value(3).toInt());
+    // Build Event 3
+    Event event3(3);
+    event3.setDate(query.value(0).toString());
+    event3.setEvent(query.value(1).toString());
+    event3.setCost(query.value(2).toFloat());
+    event3.setIncrement(query.value(3).toFloat());
+    event3.setCategory(query.value(4).toString());
+    event3.setComment(query.value(5).toString());
+    event3.setItemId(query.value(6).toInt());
 
-//     // Start Test
-//     // Update Key
-//     event3.setKey(eventFields.value(0).toString());
-//     QVERIFY(katalogue_db.updateEventEntry(event3) == true);
+    // Start Test
+    // Update Date
+    event3.setDate(eventFields.value(0).toString());
+    QVERIFY(katalogue_db.updateEventEntry(event3) == true);
 
-//     query.exec(record3Query);
-//     query.next();
-//     QVERIFY(query.value(0).toString() == eventFields.value(0).toString());
+    query.exec(record3Query);
+    query.next();
+    QVERIFY(query.value(0).toString() == eventFields.value(0).toString());
 
-//     // Update Value
-//     event3.setValue(eventFields.value(1).toString());
-//     QVERIFY(katalogue_db.updateEventEntry(event3) == true);
+    // Update Event
+    event3.setEvent(eventFields.value(1).toString());
+    QVERIFY(katalogue_db.updateEventEntry(event3) == true);
 
-//     query.exec(record3Query);
-//     query.next();
-//     QVERIFY(query.value(1).toString() == eventFields.value(1).toString());
+    query.exec(record3Query);
+    query.next();
+    QVERIFY(query.value(1).toString() == eventFields.value(1).toString());
 
-//     // Update Category
-//     event3.setCategory(eventFields.value(2).toString());
-//     QVERIFY(katalogue_db.updateEventEntry(event3) == true);
+    // Update Cost
+    event3.setCost(eventFields.value(2).toFloat());
+    QVERIFY(katalogue_db.updateEventEntry(event3) == true);
 
-//     query.exec(record3Query);
-//     query.next();
-//     QVERIFY(query.value(2).toString() == eventFields.value(2).toString());
-// }
+    query.exec(record3Query);
+    query.next();
+    QVERIFY(QString::asprintf("%.2f", query.value(2).toFloat()) == eventFields.value(2));
+
+    // Update Increment
+    event3.setIncrement(eventFields.value(3).toFloat());
+    QVERIFY(katalogue_db.updateEventEntry(event3) == true);
+
+    query.exec(record3Query);
+    query.next();
+    QVERIFY(QString::asprintf("%.1f", query.value(3).toFloat()) == eventFields.value(3));
+
+    // Update Category
+    event3.setCategory(eventFields.value(4).toString());
+    QVERIFY(katalogue_db.updateEventEntry(event3) == true);
+
+    query.exec(record3Query);
+    query.next();
+    QVERIFY(query.value(4).toString() == eventFields.value(4).toString());
+
+    // Update Comment
+    event3.setComment(eventFields.value(5).toString());
+    QVERIFY(katalogue_db.updateEventEntry(event3) == true);
+
+    query.exec(record3Query);
+    query.next();
+    QVERIFY(query.value(5).toString() == eventFields.value(5).toString());
+}
 
 // void DatabaseEventTest::deleteEventEntry() const
 // {
